9d7e58e2149799aedeb6e241fd86901bbfdd89d3
esi-lang <et178376@students.cavendish.ac.ug>
2026-04-06 01:04:59 +0300

    design and implementation of  the logic for numeric tokenization
    This involved ensuring the Lexical Analyzer could accurately distinguish between different numeric data types, which is essential for the language’s Strongly Typed and Statically Typed nature.

---
+++ next_token.py
@@ -0,0 +1,26 @@
+def next_token(self):
+        self.skip_whitespace()
+        if self.pos >= self.length:
+            return None
+        
+        char = self.source[self.pos]
+
+        
+        if char.isdigit():
+            start = self.pos
+            has_decimal = False
+            while self.pos < self.length:
+                current = self.source[self.pos]
+                if current.isdigit():
+                    self.pos += 1
+                elif current == '.' and not has_decimal:
+                    has_decimal = True # Found a decimal point
+                    self.pos += 1
+                else:
+                    break
+            
+            num_str = self.source[start:self.pos]
+        
+            if has_decimal:
+                return ('FLOAT', float(num_str))
+            return ('INTEGER', int(num_str))
