4d98729cc8d4d1e58daba6d5d7e85ec778f93da3
esi-lang <et178376@students.cavendish.ac.ug>
2026-04-30 15:58:11 +0300

    Delete lexer_part.py

--- lexer_part.py
+++
@@ -1,9 +0,0 @@
-class DataCoreLexer:
-    def __init__(self, source):
-        self.source = source
-        self.pos = 0
-        self.length = len(source)
-
-    def skip_whitespace(self):
-        while self.pos < self.length and self.source[self.pos].isspace():
-            self.pos += 1
